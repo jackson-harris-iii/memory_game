586a796b2508d614b41f56400f71d0835f576060
Apollo <Apollo@Apollo.attlocal.net>
2018-06-11 02:07:10 -0700

    create css grid wrapper for character cards

---
+++ app/src/components/Wrapper/Wrapper.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+
+
+const styles = theme => ({
+    root: {
+        flexGrow: 1,
+    },
+    paper: {
+        padding: theme.spacing.unit * 2,
+        textAlign: 'center',
+        color: theme.palette.text.secondary,
+    },
+});
+
+function AutoGrid(props) {
+    const { classes } = props;
+
+    return (
+        <div className={classes.root}>
+            <Grid container spacing={16}>
+            {props.children}
+            </Grid>
+        </div>
+    );
+}
+
+AutoGrid.propTypes = {
+    classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(AutoGrid);
