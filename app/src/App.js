cd444df03fcb3f3421126dede184d4d199c940c0
Apollo <Apollo@Apollo.lan>
2018-06-11 13:56:41 -0700

    add characters to array as they are cliked, bug - app breaks on twice clicked characters

--- app/src/App.js
+++ app/src/App.js
@@ -14,22 +14,52 @@ class App extends Component {
   }
 
   componentWillMount() {
+    //shuffles characters on initial page load.
     this.setState({ characters: this.__shuffle(this.state.characters) })
   }
 
-  updateScore = () => {
-    this.setState({ score: this.state.score + 1})
-    this.setState({ characters: this.__shuffle(this.state.characters) });
+  updateScore = id => {
+    let clickedCharacter = this.state.characters.find((character) => character.id == id);
+    console.log(this.state.clickedCharacters)
+    //checks to see if characters have been clicked previously
+    if (this.__inArray(clickedCharacter, this.state.clickedCharacters)){
+      this.setState(
+        { score: 0 }, 
+        { clickedCharacters: [] }, 
+        { characters: this.__shuffle(this.state.characters) }
+      )
+    }
+    else {
+      let alreadyClicked = this.state.clickedCharacters
+      alreadyClicked.push(clickedCharacter);
+      this.setState({ clickedCharacters: alreadyClicked })
+      console.log(this.state.clickedCharacters)
+      //updates score on character click
+      this.setState({ score: this.state.score + 1 })
+
+      //shuffles characters on character click
+      this.setState({ characters: this.__shuffle(this.state.characters) });
+    }
   }
 
   __shuffle = (a) => {
-    console.log(a)
     for (let i = a.length - 1; i > 0; i--) {
         const j = Math.floor(Math.random() * (i + 1));
         [a[i], a[j]] = [a[j], a[i]];
     }
     return a
 }
+
+  __inArray = (obj, list) => {
+    var i;
+    for (i = 0; i < list.length; i++) {
+      if (list[i] === obj) {
+        return true;
+      }
+    }
+
+    return false;
+  }
 
   render() {
     return <div className="App">
